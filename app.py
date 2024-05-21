4f803278dd9c18be88c1f24f3c80eb651534ada0
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-21 12:02:22 -0300

    routes - feat: finalized creating users routes

--- app.py
+++ app.py
@@ -1,13 +1,121 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from database import db
 from models.user import User
 from models.meal import Meal
+from datetime import datetime
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "api-daily-diet-flask"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-daily-diet'
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
+# view login
+login_manager.login_view = 'login'
+
+# PING
+@app.route("/", methods=["GET"])
+def ping():
+    return jsonify({ "enviroment": "dev", "datetime": datetime.now() })
+
+
+# LOGIN
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login",methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({ "message": "Autenticação realizada com sucesso" }), 200
+    
+    return jsonify({ "message": "Credenciais inválidas" }), 400
+
+@app.route("/logout",methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({ "message": "Logout realizado com sucesso!" })
+
+# USERS
+
+@app.route("/user",methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user")
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({ "message": "Usuário cadastrado com sucesso" })
+
+    return jsonify({ "message": "Dados inválidos!" }), 400
+
+@app.route("/user/<int:id_user>",methods=["GET"])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return { "username": user.username }
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route("/user/<int:id_user>",methods=["PUT"])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if id_user != current_user.id and current_user.role == "user": 
+        return jsonify({ "message": "Operação não permitida" }), 403
+    
+    if user and data.get("password"):
+        hashed_password = bcrypt.hashpw(str.encode(data.get("password")), bcrypt.gensalt())
+        user.password = hashed_password
+        db.session.commit()
+
+        return jsonify({ "message": f"Usuário {id_user} atualizado com sucesso" })
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route("/user/<int:id_user>",methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if current_user.role != "admin":
+        return jsonify({ "message": "Operação não permitida" }),403
+
+    if id_user == current_user.id:
+        return jsonify({ "message": "Deleção não permitida" }), 403
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({ "message": f"Usuário {id_user} deletado com sucesso" })
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
+
+
+    # MEALS
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
 from database import db
 from sqlalchemy.dialects.mysql import DATETIME
 from sqlalchemy.sql import func
+from flask_login import UserMixin
 
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(80), nullable=False,unique=True)
-    email = db.Column(db.String(80),nullable=False)
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default='user')
     created_at = db.Column(DATETIME, nullable=False, default=func.now())
     meals = db.relationship("Meal", backref="user", order_by="Meal.id")
