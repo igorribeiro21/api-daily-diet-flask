1ea781df4bd35928295cb702411cc583c21c971c
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-21 10:42:17 -0300

    structure - feat: finalized structure api and database

---
+++ models/meal.py
@@ -0,0 +1,12 @@
+from database import db
+from sqlalchemy.dialects.mysql import DATETIME, BOOLEAN
+from sqlalchemy.sql import func
+
+class Meal(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80), nullable=False,unique=True)
+    description = db.Column(db.String(80),nullable=False)
+    created_at = db.Column(DATETIME, nullable=False, default=func.now())
+    inside_diet = db.Column(BOOLEAN, nullable=False)
+    user_id = db.Column(db.ForeignKey('user.id'))
+    
